7bc4afe8d8dbd34146b9155c10bbf867484e234d
climbr88 <50963894+climbr88@users.noreply.github.com>
2020-09-29 18:29:50 -0400

    updating logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -78,7 +78,7 @@ d3.json("all_over90.json").then(function(data){
         labels: [1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009],
         datasets: [{ 
           label: result.results[1].city_name,
-          backgroundColor: "b8a9c9",
+          backgroundColor: "#77a8a8",
           data: first_line,
                               
         }, { 
